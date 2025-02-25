412c28730301a426ad46f185a11453d5c9a7100c
Svierlik-Viktor <brothervik@gmail.com>
2025-02-25 18:07:41 +0200

    All project

---
+++ src/main/java/com/cryptoapp/cryptotrainingapp/CryptoTrainingAppApplication.java
@@ -0,0 +1,13 @@
+package com.cryptoapp.cryptotrainingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoTrainingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoTrainingAppApplication.class, args);
+    }
+
+}
